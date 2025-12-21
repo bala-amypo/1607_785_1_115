cb6df87407a3314d32d770f640728f7492c750f4
bala-amypo <bala@amypo.in>
2025-12-21 09:08:36 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -3,23 +3,19 @@ package com.example.demo.service.impl;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository repository;
-    private final PasswordEncoder encoder;
 
-    public UserServiceImpl(UserRepository repository, PasswordEncoder encoder) {
+    public UserServiceImpl(UserRepository repository) {
         this.repository = repository;
-        this.encoder = encoder;
     }
 
     @Override
     public User register(User user) {
-        user.setPassword(encoder.encode(user.getPassword()));
         return repository.save(user);
     }
 }
